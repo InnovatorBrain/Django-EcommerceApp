5ef757022c569cd1b0de01b6caa95fc0c08ad95d
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2023-09-06 14:50:42 +0500

    deserializing

--- store/serializer.py
+++ store/serializer.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
+from .models import Product, Collection
+from decimal import Decimal
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ["id", "title"]
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.FloatField()
+class ProductSerializer(serializers.ModelSerializer):
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
+    price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(),
+        view_name="collection-detail"
+    )
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'price', 'price_with_tax', 'collection']
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
+
--- store/views.py
+++ store/views.py
@@ -3,17 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
+
+# from rest_framework.response import Response
 from rest_framework.response import Response
-
-from .models import Product
+from .models import Product, Collection
 from .serializer import ProductSerializer
+from .serializer import CollectionSerializer
+
+from rest_framework import status
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def product_list(request):
-    product = Product.objects.all()
-    serializer = ProductSerializer(product, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        products = Product.objects.select_related("collection").all()
+        serializer = ProductSerializer(products, many=True, context={"request": request})
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # Save the validated data to create a new Product instance
+            return Response("Product created successfully", status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view()
@@ -24,3 +36,10 @@ def product_detail(request, id):
         return Response(serializer.data)
     except Product.DoesNotExist:
         return Response(status=404)
+
+
+@api_view()
+def collection_detail(request, pk):
+    collection = Collection.objects.get(pk=pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)
