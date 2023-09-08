38f371ea32b883e2a45e5306007678f04341d631
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2023-09-08 09:09:15 +0500

    Build Restful API-1

--- store/views.py
+++ store/views.py
@@ -1,45 +1,78 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
-
-# from rest_framework.response import Response
 from rest_framework.response import Response
 from .models import Product, Collection
 from .serializer import ProductSerializer
 from .serializer import CollectionSerializer
-
 from rest_framework import status
-
 
 @api_view(["GET", "POST"])
 def product_list(request):
     if request.method == "GET":
         products = Product.objects.select_related("collection").all()
-        serializer = ProductSerializer(products, many=True, context={"request": request})
+        serializer = ProductSerializer(
+            products, many=True, context={"request": request}
+        )
         return Response(serializer.data)
     elif request.method == "POST":
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()  # Save the validated data to create a new Product instance
+            serializer.save()
             return Response("Product created successfully", status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view()
+@api_view(["GET", "PUT", 'DELETE'])
 def product_detail(request, id):
-    try:
-        product = Product.objects.get(pk=id)
-        serializer = ProductSerializer(product)
+    product = Product.objects.get(pk=id)
+    if request.method == "GET":
+        serializer = ProductSerializer(product, context={'request': request})
         return Response(serializer.data)
-    except Product.DoesNotExist:
-        return Response(status=404)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        product.delete()
+        return Response("Product deleted successfully", status=status.HTTP_204_NO_CONTENT)
 
+@api_view(["GET", "POST"])
 
-@api_view()
+def collection_list(request):
+    if request.method == "GET":
+        collections = Collection.objects.all()
+        serializer = CollectionSerializer(collections, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = CollectionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Collection created successfully", status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "PUT", "DELETE"])
 def collection_detail(request, pk):
-    collection = Collection.objects.get(pk=pk)
-    serializer = CollectionSerializer(collection)
-    return Response(serializer.data)
+    if request.method == "GET":
+        collection = Collection.objects.get(pk=pk)
+        serializer = CollectionSerializer(collection, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        collection = Collection.objects.get(pk=pk)
+        serializer = CollectionSerializer(collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        collection = Collection.objects.get(pk=pk)
+        collection.delete()
+        return Response("Collection deleted successfully", status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
