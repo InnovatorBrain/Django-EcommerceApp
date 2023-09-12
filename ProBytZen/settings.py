2195ce7b1c042e2908ec09192bfb82101bea0813
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2023-09-12 19:59:17 +0500

    Extending the User Model:

--- ProBytZen/settings.py
+++ ProBytZen/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "likes",
     "store",
     "tags",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -150,3 +151,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
+AUTH_USER_MODEL = "core.User"
